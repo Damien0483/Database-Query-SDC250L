9b976dae36fc8264406c9a2082dac6c34459950f
Damien0483 <damhar2570@students.ecpi.edu>
2025-06-22 10:50:41 -0400

    Update VerifyDBState.sql
    Putting notes into script

--- VerifyDBState.sql
+++ VerifyDBState.sql
@@ -1,62 +1,62 @@
-COMMIT;
+COMMIT; -- Initiate Saving 
 
-SELECT USER_ID
+SELECT USER_ID -- Display users that have access to ORACLE APEX
 , USERNAME
 , CREATED
 , PASSWORD_CHANGE_DATE
 FROM 
 USER_USERS;
 
-SELECT *
+SELECT *  -- Display all fields of all tables 
 FROM USER_TABLES;
 
-DESC ORDERS;
-DESC USERLIBRARY;
-DESC USERBASE;
-DESC REVIEWS;
-DESC STOREFRONT;
-DESC PRODUCTLIST;
+DESC ORDERS; -- Describe the layout of the ORDERS table
+DESC USERLIBRARY; -- Describe the layout of the USERLIBRARY table
+DESC USERBASE; -- Describe the layout of the USERBASE table
+DESC REVIEWS; -- Describe the layout of the REVIEWS table
+DESC STOREFRONT; -- Describe the layout of the STOREFRONT table
+DESC PRODUCTLIST; -- Describe the layout of the PRODUCTLIST table
 
-SELECT *
+SELECT * -- Display all fields of ORDERS table
 FROM ORDERS;
 
-SELECT *
+SELECT * -- Display all fields of PRODUCTLIST table
 FROM PRODUCTLIST;
 
-SELECT *
+SELECT * -- Display all fields of REVIEWS table
 FROM REVIEWS;
 
-SELECT *
+SELECT * -- Display all fields of STOREFRONT table
 FROM STOREFRONT;
 
-SELECT *
+SELECT * -- Display all fields of USERBASE table
 FROM USERBASE;
 
-SELECT *
+SELECT * -- Display all fields of USERLIBRARY table
 FROM USERLIBRARY;
 
-SELECT 
+SELECT -- Displaying contraint details
 TABLE_NAME
 , CONSTRAINT_NAME
 , CONSTRAINT_TYPE
 , STATUS
 FROM 
 USER_CONSTRAINTS;
--- Start Here Checking Code in Sandbox
-SELECT 
+
+SELECT -- Display user view information 
 VIEW_NAME
 , TEXT
 FROM 
 USER_VIEWS;
 
-SELECT 
+SELECT -- Display all users by username in ascending order
 USERNAME
 FROM 
 USERBASE
 ORDER BY
 USERNAME ASC;
 
-SELECT 
+SELECT -- Displaying all users with a yahoo email account
 FIRSTNAME
 , LASTNAME
 , USERNAME
@@ -66,7 +66,7 @@ FROM
 USERBASE
 WHERE EMAIL LIKE '%@yahoo.com';
 
-SELECT
+SELECT -- Displaying users with a balance of less than $25 
 USERNAME
 , BIRTHDAY
 , WALLETFUNDS
@@ -74,25 +74,25 @@ FROM
 USERBASE
 WHERE WALLETFUNDS < 25;
 
-SELECT
+SELECT -- Display users who have played mored than 100 hours and the product
 USERID
 , PRODUCTCODE
 FROM 
 USERLIBRARY
 WHERE HOURSPLAYED > 100;
 
-SELECT 
+SELECT -- Displaying users who have played less than 10 hours and the product
 PRODUCTCODE
 FROM
 USERLIBRARY
 WHERE HOURSPLAYED < 10;
 
-SELECT DISTINCT
+SELECT DISTINCT -- Showing all unique publishers no duplicate entries
 PUBLISHER
 FROM 
 PRODUCTLIST;
 
-SELECT
+SELECT -- Displaying product information by genre 
 PRODUCTNAME
 , RELEASEDATE
 , PUBLISHER
@@ -102,14 +102,14 @@ PRODUCTLIST
 ORDER BY 
 GENRE;
 
-SELECT
+SELECT -- Displaying only strategy genre games
 PRODUCTNAME
 , PUBLISHER
 FROM
 PRODUCTLIST
 WHERE GENRE LIKE 'Strategy';
 
-SELECT
+SELECT -- Displaying all games that cost more than $25 and sorting by descending price
 PRODUCTCODE
 , DESCRIPTION
 , PRICE
@@ -119,36 +119,36 @@ WHERE
 PRICE > 25
 ORDER BY PRICE DESC;
 
-SELECT
+SELECT -- Displaying items in inventory and sorting by ascending price
 INVENTORYID
 , PRICE
 FROM
 STOREFRONT
 ORDER BY PRICE ASC;
 
-SELECT 
+SELECT -- Displaying products by code and displaying reviews with a rating of 1
 PRODUCTCODE
 , REVIEW
 FROM
 REVIEWS
 WHERE RATING = 1;
 
-SELECT 
+SELECT -- Displaying products by code and displaying reviews with a rating of 4 or greater
 PRODUCTCODE
 , REVIEW
 FROM
 REVIEWS
 WHERE RATING >= 4;
 
-SELECT DISTINCT 
+SELECT DISTINCT -- Displaying all users with orders with no duplicate entries
 USERID
 FROM 
 ORDERS;
 
-SELECT *
+SELECT * -- Displaying all fields in the ORDERS table and sorting by ascending purchase date
 FROM 
 ORDERS
 ORDER BY 
 PURCHASEDATE ASC;
 
-COMMIT;
+COMMIT; -- Initiating save
